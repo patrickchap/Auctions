6e27e74cd2239802d16e97cf1f9d852a8661edf5
Patrick <patrick.chapple28@icloud.com>
2025-03-29 00:42:55 -0600

    add entity setup db

---
+++ src/AUCTIONS.SearchService/Entities/Item.cs
@@ -0,0 +1,22 @@
+
+using MongoDB.Entities;
+namespace SearchService.Entities;
+
+public class Item : Entity
+{
+    public int ReservePrice { get; set; }
+    public required string Seller { get; set; }
+    public string Winner { get; set; } = string.Empty;
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public string Make { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string Color { get; set; } = string.Empty;
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+}
--- src/AUCTIONS.SearchService/Program.cs
+++ src/AUCTIONS.SearchService/Program.cs
@@ -1,8 +1,14 @@
+using MongoDB.Entities;
+using MongoDB.Driver;
+using SearchService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
@@ -19,4 +25,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await DB.InitAsync("SearchDB"
+, MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnectionString")));
+
+await DB.Index<Item>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.Model, KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
 app.Run();
